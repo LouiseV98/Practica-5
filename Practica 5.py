e98705991aaeba220e1408fb03441496dd4d6a69
LouiseV98 <143037181+LouiseV98@users.noreply.github.com>
2023-10-04 14:59:09 -0600

    FInal
    Hice muchos cambios, es la ultima modificacion

--- Practica 5.py
+++ Practica 5.py
@@ -1,53 +1,161 @@
-def extract_size(size_str):
-    # Eliminar "kb" y convertir a entero
-    return int(size_str.replace("kb", ""))
+def mejor_Ajuste(tamano_Proceso, espacio_Memoria, procesos_Memoria):    #Funcion del algoritmo del mejor ajuste
+    mejor_Ajuste_Idx = -1
+    espacio_Libre = float('inf')
 
-def first_fit(memory, process_name, process_size):
-    for i in range(len(memory)):
-        if memory[i] is None:
-            if allocate(memory, i, process_size, process_name):
-                return i
-    return -1
+    for i in range(len(espacio_Memoria)):
+        if espacio_Memoria[i] >= tamano_Proceso and espacio_Memoria[i] - tamano_Proceso < espacio_Libre:
+            mejor_Ajuste_Idx = i
+            espacio_Libre = espacio_Memoria[i] - tamano_Proceso
 
-def allocate(memory, process_name, process_size, memory_sizes, block_start):
-    for i in range(block_start, len(memory_sizes)):
-        if process_size <= memory_sizes[i]:
-            for j in range(len(memory)):
-                if memory[j] is None and j + process_size <= len(memory):
-                    block_free = True
-                    for k in range(j, j + process_size):
-                        if memory[k] is not None:
-                            block_free = False
-                            break
-                    if block_free:
-                        for k in range(j, j + process_size):
-                            memory[k] = process_name
-                        return i  # Devuelve el índice del bloque de memory_sizes asignado
-    return -1  # Devuelve -1 si no se pudo asignar
+    if mejor_Ajuste_Idx != -1:
+        procesos_Memoria.append(tamano_Proceso)
+        espacio_Memoria[mejor_Ajuste_Idx] -= tamano_Proceso
+        print(f"Proceso de {tamano_Proceso} KB asignado al espacio {mejor_Ajuste_Idx}")
+    else:
+        print(f"Proceso {proceso}: Tamaño {tamano_Proceso} KB no hay suficiente espacio en la memoria")
 
-def algoritmo_Primer_Ajuste():
-    memory_sizes = [1000, 400, 1800, 700, 900, 1200, 1500]  # Tamaños de los bloques de memoria en KB
-    memory = [None] * sum(memory_sizes)  # Crear la lista de memoria con la suma de los tamaños
+def primer_Ajuste(tamano_Proceso, espacios_Memoria):    #Funcion del algoritmo del primer ajuste
+    for i in range(len(espacios_Memoria)):
+        if espacios_Memoria[i] >= tamano_Proceso:
+            print(f"Proceso de {tamano_Proceso} KB asignado al espacio {i}")
+            espacios_Memoria[i] -= tamano_Proceso
+            return True
+    return False
 
-    # Procesar el archivo con nombres de archivos y tamaños
-    with open("archivos.txt", "r") as file:
-        block_start = 0
-        for line in file:
-            line = line.strip()
-            process_name, process_size_str = line.split(",")
-            process_name = process_name.strip()
-            process_size = int(process_size_str.replace("kb", ""))
-            allocated_index = allocate(memory, process_name, process_size, memory_sizes, block_start)
-            if allocated_index != -1:
-                print(f"Proceso '{process_name}' asignado al bloque {allocated_index}.")
-                block_start = allocated_index + 1  # Actualiza el inicio del bloque
-            else:
-                print(f"No se pudo asignar el proceso '{process_name}' debido a la falta de memoria.")
+def peor_Ajuste(espacios_Memoria, tamano_Proceso):  #Funcion del algoritmo del peor ajuste
+    for proceso in tamano_Proceso:
+        peor_Ajuste_Index = -1
+        peor_Ajuste_Tamano = -1
 
-if __name__ == "__main__":
-    algoritmo_Primer_Ajuste()
+        for i in range(len(espacios_Memoria)):
+            if espacios_Memoria[i] >= proceso[1]:
+                if peor_Ajuste_Index == -1 or espacios_Memoria[i] > espacios_Memoria[peor_Ajuste_Index]:
+                    peor_Ajuste_Index = i
+                    peor_Ajuste_Tamano = espacios_Memoria[i]
 
+        if peor_Ajuste_Index != -1:
+            espacios_Memoria[peor_Ajuste_Index] -= proceso[1]
+            print(f"Procesando: {proceso[0]} ({proceso[1]} KB)")
+            print(f"Proceso de {proceso[1]} KB asignado en el espacio {peor_Ajuste_Index}")
+        else:
+            print(f"Procesando: {proceso[0]} ({proceso[1]} KB)")
+            print(f"Proceso {proceso[0]}: Tamaño {proceso[1]} KB no hay suficiente espacio en la memoria")
 
+def siguiente_Ajuste(espacios_Memoria, tamano_Proceso): #Funcion del algoritmo del siguiente ajuste
+    ultimo_Indice_Asignado = 0
 
+    for proceso in tamano_Proceso:
+        proceso_Asignado = False
 
+        for i in range(ultimo_Indice_Asignado, len(espacios_Memoria)):
+            if espacios_Memoria[i] >= proceso[1]:
+                espacios_Memoria[i] -= proceso[1]
+                print(f"Procesando: {proceso[0]} ({proceso[1]} KB)")
+                print(f"Proceso de {proceso[1]} KB asignado en el espacio {i}")
+                proceso_Asignado = True
+                ultimo_Indice_Asignado = i + 1
+                break
 
+        if not proceso_Asignado:
+            print(f"Procesando: {proceso[0]} ({proceso[1]} KB)")
+            print(f"No se pudo asignar el proceso {proceso[0]} de {proceso[1]} KB")
+
+#Espacios de memoria para los algoritmos
+espacio_Memoria1 = [1000, 400, 1800, 700, 900, 1200, 1500]
+espacio_Memoria2 = [1000, 400, 1800, 700, 900, 1200, 1500]
+espacio_Memoria3 = [1000, 400, 1800, 700, 900, 1200, 1500]
+espacio_Memoria4 = [1000, 400, 1800, 700, 900, 1200, 1500]
+
+#Menu del programa
+while True:
+    print("\n\tMenu\n")
+    print("Espacio de Memoria: [1000, 400, 1800, 700, 900, 1200, 1500]\n")
+    print("Selecciona un algoritmo de asignación de memoria:")
+    print("1. Mejor Ajuste")
+    print("2. Primer Ajuste")
+    print("3. Peor Ajuste")
+    print("4. Siguiente Ajuste")
+    print("5. Salir")
+
+    opcion = input("Ingresa el número de la opción deseada: ")
+
+    if opcion == '1':
+        print("\n\tMejor Ajuste\n")
+        with open("archivos.txt", "r") as archivo:
+            lineas = archivo.readlines()
+
+        procesos_en_Memoria = []
+        for linea in lineas:
+            proceso, tamano = linea.strip().split(', ')
+            tamano_Proceso = int(tamano.replace('kb', ''))
+            print(f"Procesando: {proceso} ({tamano_Proceso} KB)")
+            mejor_Ajuste(tamano_Proceso, espacio_Memoria1, procesos_en_Memoria)
+
+        # Estado actual de la memoria después de asignar procesos
+        print("\n\tEstado actual de la memoria:\n")
+        for i in range(len(espacio_Memoria1)):
+            print(f"Espacio {i}: {espacio_Memoria1[i]} KB")
+
+    elif opcion == '2':
+        print("\n\tPrimer Ajuste\n")
+        with open('archivos.txt', 'r') as file:
+            lineas = file.readlines()
+
+        procesos_en_Memoria = []
+        for linea in lineas:
+            proceso, tamano = linea.strip().split(', ')
+            tamano = int(tamano[:-2])
+            procesos_en_Memoria.append((proceso, tamano))
+            
+        for proceso, tamano in procesos_en_Memoria:
+            print(f"Procesando: {proceso} ({tamano} KB)")
+            if not primer_Ajuste(tamano, espacio_Memoria2):
+                print(f"Proceso {proceso}: Tamaño {tamano} KB no hay suficiente espacio en la memoria")
+        
+        # Estado actual de la memoria después de asignar procesos
+        print("\n\tEstado actual de la memoria:\n")
+        for i in range(len(espacio_Memoria2)):
+            print(f"Espacio {i}: {espacio_Memoria2[i]} KB")
+
+    elif opcion == '3':
+        print("\n\tPeor Ajuste\n")
+        with open("archivos.txt", "r") as archivo:
+            lineas = archivo.readlines()
+
+        procesos_en_Memoria = []
+        for linea in lineas:
+            proceso = linea.strip().split(", ")
+            nombre_Proceso = proceso[0]
+            tamano_Proceso = int(proceso[1].replace("kb", ""))
+            procesos_en_Memoria.append((nombre_Proceso, tamano_Proceso))
+
+        peor_Ajuste(espacio_Memoria3, procesos_en_Memoria)
+
+        # Estado actual de la memoria después de asignar procesos
+        print("\n\tEstado actual de la memoria:\n")
+        for i in range(len(espacio_Memoria3)):
+            print(f"Espacio {i}: {espacio_Memoria3[i]} KB")
+
+    elif opcion == '4':
+        print("\n\tSiguiente Ajuste\n")
+        with open("archivos.txt", "r") as archivo:
+            lineas = archivo.readlines()
+
+        procesos_en_Memoria = []
+        for linea in lineas:
+            proceso = linea.strip().split(", ")
+            nombre_Proceso = proceso[0]
+            tamano_Proceso = int(proceso[1].replace("kb", ""))
+            procesos_en_Memoria.append((nombre_Proceso, tamano_Proceso))
+
+        siguiente_Ajuste(espacio_Memoria4, procesos_en_Memoria)
+
+        # Estado actual de la memoria después de asignar procesos
+        print("\n\tEstado actual de la memoria:\n")
+        for i in range(len(espacio_Memoria4)):
+            print(f"Espacio {i}: {espacio_Memoria4[i]} KB")
+
+    elif opcion == '5':
+        break
+    else:
+        print("Opción no válida. Por favor, elige una opción válida.")
